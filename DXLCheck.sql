39badab9511a899f44d67d6bacf341f32900a0db
ryoichi.naito@thomsonreuters.com <hotryo123@yahoo.co.jp>
2016-11-16 17:32:03 +0900

    updated

--- DXLCheck.sql
+++ DXLCheck.sql
@@ -1,6 +1,11 @@
 
 
+-- Item list
 select top 10000 * from DXLItem
 
-select top 100 * from DXLJpData # Japanese Security-Lending Data Items
+-- Japanese Security-Lending Data Items
+select top 100 * from DXLJpData
 
+-- Security information
+-- DataRegionCode: 1=All regions, 5=Japan, 2=Asia ex Japan, 3=Australia, 4=Europe, 6=North America, 7=Others
+select top 10000 * from DXLSecInfo where DataRegionCode=5 order by Quick
