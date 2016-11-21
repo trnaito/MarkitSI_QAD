36693b1f7492b3dcacbe6bae03af0caffabb81c8
ryoichi.naito@thomsonreuters.com <hotryo123@yahoo.co.jp>
2016-11-21 16:15:13 +0900

    updated

--- DXLCheck.sql
+++ DXLCheck.sql
@@ -10,10 +10,13 @@ select top 10000 * from DXLItem
 select top 10000 * from DXLSecInfo where DataRegionCode=5 and Quick is not null order by Quick
 
 select top 10000
- *
+  jp.*
+, it.*
 from
-    DXLJpData -- Japanese Security-Lending Data Items
+    DXLJpData jp -- Japanese Security-Lending Data Items
+	join DXLItem it on jp.Item = it.Item 
 where
-    Code = 2273 -- Toyota
+    jp.Code = 2273 -- Toyota
 	--and Item=1 -- Total Demand Value
-	and Item=7 -- Utilization
+	--and Item=7 -- Utilization
+	and jp.Date_ = '2016-11-16'
