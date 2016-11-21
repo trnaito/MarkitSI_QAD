50e95fd2c4ff4f8a3676dad25c766902503c849a
ryoichi.naito@thomsonreuters.com <hotryo123@yahoo.co.jp>
2016-11-21 13:37:48 +0900

    updated

--- DXLCheck.sql
+++ DXLCheck.sql
@@ -3,9 +3,17 @@
 -- Item list
 select top 10000 * from DXLItem
 
--- Japanese Security-Lending Data Items
-select top 100 * from DXLJpData
+
 
 -- Security information
 -- DataRegionCode: 1=All regions, 5=Japan, 2=Asia ex Japan, 3=Australia, 4=Europe, 6=North America, 7=Others
-select top 10000 * from DXLSecInfo where DataRegionCode=5 order by Quick
+select top 10000 * from DXLSecInfo where DataRegionCode=5 and Quick is not null order by Quick
+
+select top 10000
+ *
+from
+    DXLJpData -- Japanese Security-Lending Data Items
+where
+    Code = 2273 -- Toyota
+	--and Item=1 -- Total Demand Value
+	and Item=7 -- Utilization
